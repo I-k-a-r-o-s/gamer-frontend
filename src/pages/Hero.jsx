9bac814a0127b4391e47e3c48bba63892184bc73
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-23 10:46:48 +0530

    restructuring

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,6 +1,7 @@
 import GameCard from "../components/GameCard";
 
 const Hero = () => {
+  const gamesNumbers = [1, 2, 3, 4, 5];
   return (
     <div className="p-4">
       <div className="grid grid-cols-2 gap-4 m-4">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,16 @@
 import GameCard from "../components/GameCard";
-import Navbar from "../components/Navbar";
-import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   const gamesNumbers = [1, 2, 3, 4, 5];
   return (
     <div className="drawer lg:drawer-open">
-      <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content">
-        {/* Navbar */}
-        <Navbar />
-        {/* Page content here */}
-        <div className="p-4">
-          <div className="grid grid-cols-2 gap-4 m-4">
-            {gamesNumbers.map((number) => (
-              <GameCard key={number} />
-            ))}
-          </div>
+      <div className="p-4">
+        <div className="grid grid-cols-2 gap-4 m-4">
+          {gamesNumbers.map((number) => (
+            <GameCard key={number} />
+          ))}
         </div>
       </div>
-      {/* Sidebar */}
-      <Sidebar />
     </div>
   );
 };
