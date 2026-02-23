0718b96fc3abb7b94e80a93f6b711ce34a4a1520
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-23 12:40:16 +0530

    context wrap - again

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import GamerContextProvider from "./context/GamerContext.jsx";
+import AuthContextProvider from "./context/AuthContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GamerContextProvider>
-      <App />
-    </GamerContextProvider>
+    <AuthContextProvider>
+      <GamerContextProvider>
+        <App />
+      </GamerContextProvider>
+    </AuthContextProvider>
   </StrictMode>,
 );
