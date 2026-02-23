5e6841511eaa186fee1d7ffe80e31ad998cb3c15
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-23 12:40:53 +0530

    basic card design

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
+import { MdFavoriteBorder, MdOutlineFavorite } from "react-icons/md";
+
 const GameCard = () => {
+  const [favorite, setFavorite] = useState(false);
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      Card Title
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <a href="#" className="hover-3d my-12 mx-2 cursor-pointer">
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+            className="rounded-xl"
+          />
+        </figure>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </a>
+      <div className="card-body">
+        <h2 className="card-title">
+          Card Title          
+        </h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions justify-end">  
+          <button
+              className="btn btn-circle"
+              onClick={() => setFavorite(!favorite)}
+            >
+              {favorite ? (
+                <MdOutlineFavorite size={20} />
+              ) : (
+                <MdFavoriteBorder size={20} />
+              )}
+            </button>      
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
-export default GameCard
+  );
+};
+export default GameCard;
