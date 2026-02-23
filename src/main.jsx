65f6ba08eb272447bbf9d6b3af0c2497a372e9d8
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-23 10:51:05 +0530

    wrapped the main app with the context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import GamerContextProvider from "./context/GamerContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <GamerContextProvider>
+      <App />
+    </GamerContextProvider>
   </StrictMode>,
-)
+);
