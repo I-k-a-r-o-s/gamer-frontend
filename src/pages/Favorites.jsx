c8097ec73f31bc80f64285fc2a30a3d5ef04ed5c
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-24 13:09:20 +0530

    placeholder ui

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,15 @@
+const Favorites = () => {
+  const gamesNumbers = [1, 2, 3, 4, 5];
+  return (
+    <div className="p-4">
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {gamesNumbers.map((number) => (
+            <GameCard key={number} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Favorites;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,31 @@
+import { Link } from "react-router";
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
-export default Login
+    <div>
+      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input type="email" className="input" placeholder="Email" />
+
+        <label className="label">Password</label>
+        <input type="password" className="input" placeholder="Password" />
+
+        <button className="btn btn-neutral mt-4">Login</button>
+        <div className="text-center pt-6 border-t border-base-300">
+          <p className="text-base-content/70">
+            Don't have an account?{" "}
+            <Link
+              to="/register"
+              className="link link-primary font-bold hover:link-secondary transition-colors"
+            >
+              Create one now!
+            </Link>
+          </p>
+        </div>
+      </fieldset>
+    </div>
+  );
+};
+export default Login;
