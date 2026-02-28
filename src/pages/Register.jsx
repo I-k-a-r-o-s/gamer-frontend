c7f89d29c834ba1db2c5682c25a08e593f03dffc
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-02-28 11:36:56 +0530

    register page and functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,20 +4,19 @@ import { AuthContext } from "../context/AuthContext";
 import AuthApi from "../api/AuthApi";
 
 const Register = () => {
-  const { navigate } = useContext(AuthContext);
+  const { navigate, loading, setLoading } = useContext(AuthContext);
   const [formdata, setFormdata] = useState({
     name: "",
     email: "",
     password: "",
   });
-  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formdata);
 
     if (!formdata.name || !formdata.email || !formdata.password) {
       toast.error("Please fill all the fields");
+      return;
     }
 
     try {
@@ -38,50 +37,54 @@ const Register = () => {
   };
   return (
     <div>
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Register</legend>
+      <form onSubmit={handleSubmit}>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          <legend className="fieldset-legend">Register</legend>
 
-        <label className="label">Name</label>
-        <input
-          type="text"
-          className="input"
-          placeholder="Name"
-          value={formdata.name}
-          onChange={(e) => setFormdata({ ...formdata, name: e.target.value })}
-        />
+          <label className="label">Name</label>
+          <input
+            type="text"
+            className="input"
+            placeholder="Name"
+            value={formdata.name}
+            onChange={(e) => setFormdata({ ...formdata, name: e.target.value })}
+          />
 
-        <label className="label">Email</label>
-        <input
-          type="email"
-          className="input"
-          placeholder="Email"
-          value={formdata.email}
-          onChange={(e) => setFormdata({ ...formdata, email: e.target.value })}
-        />
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input"
+            placeholder="Email"
+            value={formdata.email}
+            onChange={(e) =>
+              setFormdata({ ...formdata, email: e.target.value })
+            }
+          />
 
-        <label className="label">Password</label>
-        <input
-          type="password"
-          className="input"
-          placeholder="Password"
-          value={formdata.password}
-          onChange={(e) =>
-            setFormdata({ ...formdata, password: e.target.value })
-          }
-        />
+          <label className="label">Password</label>
+          <input
+            type="password"
+            className="input"
+            placeholder="Password"
+            value={formdata.password}
+            onChange={(e) =>
+              setFormdata({ ...formdata, password: e.target.value })
+            }
+          />
 
-        <button
-          className="btn btn-neutral mt-4"
-          onClick={handleSubmit}
-          disabled={loading}
-        >
-          {loading ? (
-            <span className="loading loading-spinner"></span>
-          ) : (
-            "Register"
-          )}
-        </button>
-      </fieldset>
+          <button
+            className="btn btn-neutral mt-4"
+            type="submit"
+            disabled={loading}
+          >
+            {loading ? (
+              <span className="loading loading-spinner"></span>
+            ) : (
+              "Register"
+            )}
+          </button>
+        </fieldset>
+      </form>
     </div>
   );
 };
