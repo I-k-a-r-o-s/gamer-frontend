d5740c56f9c12af16cdf42279342f2af41e4b982
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-06 10:24:35 +0530

    added location check to hide login button on login page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,13 @@ import {
 } from "react-icons/tb";
 import ThemeSelector from "./ThemeSelector";
 import { useState, useContext } from "react";
-import { Link } from "react-router";
+import { Link} from "react-router";
 import { AuthContext } from "../context/AuthContext";
+import { FaRegUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-  const { user, logout } = useContext(AuthContext);
+  const { user, logout,location } = useContext(AuthContext);
   return (
     <nav className="navbar w-full bg-base-300">
       <div className="navbar-start">
@@ -33,19 +34,35 @@ const Navbar = () => {
         </div>
       </div>
       <div className="navbar-end">
-        {user ? (
-          <>
-            <span className="px-4 text-sm font-semibold">{user.name}</span>
-            <button className="btn btn-ghost" onClick={logout}>
-              Logout
-            </button>
-          </>
-        ) : (
-          <Link to="/login" className="btn btn-ghost">
-            Login
-          </Link>
-        )}
         <ThemeSelector />
+        {user ? (
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="rounded-full">
+                <FaRegUserCircle size={20} />
+              </div>
+            </div>
+            <ul
+              tabIndex={-1}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a onClick={logout}>Logout</a>
+              </li>
+            </ul>
+            <span className="px-4 text-sm font-semibold">{user.name}</span>
+          </div>
+        ) : (
+          location.pathname !== "/login" && (
+            <Link to="/login" className="btn btn-ghost">
+              Login
+            </Link>
+          )
+        )}
       </div>
     </nav>
   );
